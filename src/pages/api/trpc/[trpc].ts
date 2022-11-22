cdb67aa2d322bba06dc9bd1ffc091286118e3a41
Edem Ziddah <contact@codevoweb.com>
2022-11-22 08:06:31 +0000

    disabled cors in [trpc].ts

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,35 +1,33 @@
 import * as trpcNext from "@trpc/server/adapters/next";
 import { appRouter } from "~/server/app.router";
-import Cors from "cors";
-import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
-const cors = Cors({
-  origin: [`${process.env.NEXT_PUBLIC_VERCEL_URL}`],
-  credentials: true,
+// import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+// import Cors from "cors";
+// const cors = Cors({
+//   origin: [`${process.env.NEXT_PUBLIC_VERCEL_URL}`],
+//   credentials: true,
+// });
+
+// function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result: any) => {
+//       if (result instanceof Error) {
+//         return reject(result);
+//       }
+
+//       return resolve(result);
+//     });
+//   });
+// }
+
+// export function withCors(handler: NextApiHandler) {
+//   return async (req: NextApiRequest, res: NextApiResponse) => {
+//     await runMiddleware(req, res, cors);
+
+//     return await handler(req, res);
+//   };
+// }
+
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
 });
-
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
-
-export function withCors(handler: NextApiHandler) {
-  return async (req: NextApiRequest, res: NextApiResponse) => {
-    await runMiddleware(req, res, cors);
-
-    return await handler(req, res);
-  };
-}
-
-export default withCors(
-  trpcNext.createNextApiHandler({
-    router: appRouter,
-  })
-);
