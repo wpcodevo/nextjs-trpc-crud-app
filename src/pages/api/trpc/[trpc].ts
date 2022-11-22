ac32e60d310aa2e63b2c9f5b766f58df8f8a1baa
Edem Ziddah <contact@codevoweb.com>
2022-11-22 06:08:40 +0000

    added CORS package to the tRPC API

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,33 @@
 import * as trpcNext from "@trpc/server/adapters/next";
 import { appRouter } from "~/server/app.router";
+import Cors from "cors";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
-export default trpcNext.createNextApiHandler({
+
+const cors = Cors();
+
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export function withCors(handler: NextApiHandler) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    await runMiddleware(req, res, cors);
+
+    return await handler(req, res);
+  };
+}
+
+export default withCors(
+  trpcNext.createNextApiHandler({
   router: appRouter,
-});
+})
+);
