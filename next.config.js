d34ed0a04551c11c5e6438bef1828911836eb565
Edem Ziddah <contact@codevoweb.com>
2022-11-22 06:13:39 +0000

    changed the path in the CORS headers

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
+        source: "/api/trpc/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
