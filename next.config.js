8d6b66c14a12f318bbdeb16d1d50d814da30b6f5
Edem Ziddah <contact@codevoweb.com>
2022-11-22 06:16:29 +0000

    modified the source path in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/api/trpc/:path*",
+        source: "/api/trpc/[trpc].ts",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
